e5b83df846ab3e54e67e988fcfc5dd63850f6e3b
Alex-Potlog <alexpotloghary@gmail.com>
2025-05-09 16:50:28 +0200

    cració SQLManager

--- src/grafic/MyLogin.java
+++ src/grafic/MyLogin.java
@@ -26,31 +26,30 @@ public class MyLogin extends JDialog {
         contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         getContentPane().add(contentPanel, BorderLayout.CENTER);
         contentPanel.setLayout(new BorderLayout(0, 0));
-        {
-            JLabel lblInsereixElTeu = new JLabel("Insereix el teu username");
-            contentPanel.add(lblInsereixElTeu, BorderLayout.NORTH);
-        }
-        {
-            textField = new JTextField();
-            contentPanel.add(textField, BorderLayout.CENTER);
-            textField.setColumns(10);
-        }
-        {
-            JPanel buttonPane = new JPanel();
-            buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-            getContentPane().add(buttonPane, BorderLayout.SOUTH);
-            {
-                JButton okButton = new JButton("OK");
-                okButton.setActionCommand("OK");
-                buttonPane.add(okButton);
-                getRootPane().setDefaultButton(okButton);
-            }
-            {
-                JButton cancelButton = new JButton("Cancel");
-                cancelButton.setActionCommand("Cancel");
-                buttonPane.add(cancelButton);
-            }
-        }
+
+        JLabel lblInsereixElTeu = new JLabel("Insereix el teu username");
+        contentPanel.add(lblInsereixElTeu, BorderLayout.NORTH);
+
+
+        textField = new JTextField();
+        contentPanel.add(textField, BorderLayout.CENTER);
+        textField.setColumns(10);
+
+        JPanel buttonPane = new JPanel();
+        buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        getContentPane().add(buttonPane, BorderLayout.SOUTH);
+
+        JButton okButton = new JButton("OK");
+        okButton.setActionCommand("OK");
+        buttonPane.add(okButton);
+        getRootPane().setDefaultButton(okButton);
+
+
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.setActionCommand("Cancel");
+        buttonPane.add(cancelButton);
+
+
     }
 
 }
