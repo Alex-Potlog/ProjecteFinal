4e0eede15945b36909dbf7cf3645b514d52b646a
Alex-Potlog <alexpotloghary@gmail.com>
2025-05-09 20:37:08 +0200

    Canvi titol

--- src/grafic/MyChat.java
+++ src/grafic/MyChat.java
@@ -19,6 +19,7 @@ public class MyChat extends JFrame {
      */
     public MyChat()  {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Chat");
         setBounds(100, 100, 450, 300);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
