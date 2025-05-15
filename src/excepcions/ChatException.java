cb09bbed6a022b6c459b129e08075049ff004342
Alex-Potlog <alexpotloghary@gmail.com>
2025-05-15 17:25:00 +0200

    La excepció és checked ara

--- src/excepcions/ChatException.java
+++ src/excepcions/ChatException.java
@@ -1,6 +1,6 @@
 package excepcions;
 
-public class ChatException extends RuntimeException {
+public class ChatException extends Exception {
     private static final long serialVersionUID = 1L;
     private String codi;
 
